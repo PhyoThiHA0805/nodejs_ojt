9e199935557998529e690ac166a012aa4751ad43
PhyoThiHA0805 <scm.phyothihakyaw@gmail.com>
2022-12-05 16:25:25 +0630

    includes().

--- section12.js
+++ section12.js
@@ -11,3 +11,14 @@ const title1 = 'Jack and Jill Went Up the Hill';
 console.log(title.endsWith('Hill'));
 console.log(title.endsWith('hill'));
 console.log(title.endsWith('Up', 21));
+
+
+// includes()
+let email = 'admin@example.com';
+console.log(email.includes('@'));
+
+let str = 'JavaScript String';
+
+console.log(str.includes('Script'));
+console.log(str.includes('script'));
+console.log(str.includes('Script', 5));
