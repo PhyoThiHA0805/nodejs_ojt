8db4fbca801945f1b6964bcc6041ffa8639c99d2
PhyoThiHA0805 <scm.phyothihakyaw@gmail.com>
2022-12-05 15:51:53 +0630

    Array.findIndex().

--- section10.js
+++ section10.js
@@ -83,3 +83,22 @@ customers.map(customer => {
 
 console.log(newArr);
 
+
+// Array.findIndex()
+// let ranks = [1, 5, 7, 8, 10, 7];
+
+// let index = ranks.findIndex(
+//     (rank, index) => rank === 7 && index > 2
+// );
+
+// console.log(index);
+
+const products = [
+    { name: 'Phone', price: 999 },
+    { name: 'Computer', price: 1999 },
+    { name: 'Tablet', price: 995 },
+  ];
+  
+  const index = products.findIndex(product => product.price > 1000);
+  
+  console.log(index);
