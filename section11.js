63bcca9a81174e6cca0d12e00d7022edea1372c8
PhyoThiHA0805 <scm.phyothihakyaw@gmail.com>
2022-12-05 16:06:27 +0630

    Object.is().

--- section11.js
+++ section11.js
@@ -1,3 +1,4 @@
+// Object.assign()
 let box = {
     height: 10,
     width: 20,
@@ -12,3 +13,14 @@ let style = {
 let styleBox = Object.assign({}, box, style);
 // styleBox = {...style, ...box};
 console.log(styleBox);
+
+
+// Object.is()
+let amount = +0,
+    volume = -0;
+console.log('From Operator : ', volume === amount);
+console.log('From Object.is() : ', Object.is(amount, volume));
+
+let quantity = NaN;
+console.log('From Operator : ', quantity === quantity);
+console.log('From Object.is() : ', Object.is(quantity, quantity));
