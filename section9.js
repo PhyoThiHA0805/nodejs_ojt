c763df9c926afc782bafaa3552bdce0e2671297c
PhyoThiHA0805 <scm.phyothihakyaw@gmail.com>
2022-12-05 14:31:25 +0630

    Map.

---
+++ section9.js
@@ -0,0 +1,41 @@
+let john = {name: 'John Doe'},
+    lily = {name: 'Lily Bush'},
+    peter = {name: 'Peter Drucker'},
+    foo = {name: 'Foo'};
+
+let users = [john, lily, peter, foo];
+let userRoles = new Map([
+    [john, 'admin'],
+    [lily, 'editor'],
+    [peter, 'subscriber']
+]);
+
+users.map(user => {
+    if (user.name.includes('John')) {
+        user.role = 'admin';  
+    }
+    console.log(users);
+})
+
+console.log(typeof(userRoles)); // object
+console.log(userRoles instanceof Map);
+console.log(userRoles.get(john));
+console.log(userRoles.has(foo));
+console.log(userRoles.has(lily));
+console.log(userRoles.size);
+
+userRoles.set(foo, 'viewer');
+
+console.log(userRoles, 'userRoles');
+
+for (const [user, role] of userRoles.entries()) {
+    // console.log('Entries: ', role)
+    console.log(`${user.name}: ${role}`);
+  }
+
+userRoles.forEach((role, user) => console.log(`Result from forEach =>  ${user.name} : ${role}`));
+
+userRoles.delete(john);
+userRoles.clear();
+console.log('After delete', userRoles);
+
