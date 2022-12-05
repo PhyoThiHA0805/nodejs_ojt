cd5020223d3559b6a22c35905173064d6110ec57
PhyoThiHA0805 <scm.phyothihakyaw@gmail.com>
2022-12-05 16:21:30 +0630

    endsWith().

--- section12.js
+++ section12.js
@@ -5,3 +5,9 @@ console.log(title.startsWith('Jack'));
 console.log(title.startsWith('jack'));
 console.log(title.startsWith('Jill', 9));
 
+// endswith()
+const title1 = 'Jack and Jill Went Up the Hill';
+
+console.log(title.endsWith('Hill'));
+console.log(title.endsWith('hill'));
+console.log(title.endsWith('Up', 21));
