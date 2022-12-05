b7a31dd37950fef62919ac86338616c943c2d4b0
PhyoThiHA0805 <scm.phyothihakyaw@gmail.com>
2022-12-05 16:09:58 +0630

    startsWith().

---
+++ section12.js
@@ -0,0 +1,7 @@
+// startsWith()
+const title = 'Jack and Jill Went Up the Hill';
+
+console.log(title.startsWith('Jack'));
+console.log(title.startsWith('jack'));
+console.log(title.startsWith('Jill', 9));
+
