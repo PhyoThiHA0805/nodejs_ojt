fa1e12b7bb1d09557493acde19014fd7147beae6
PhyoThiHA0805 <scm.phyothihakyaw@gmail.com>
2022-12-06 10:01:48 +0630

    Stream Update.

--- app.js
+++ app.js
@@ -110,3 +110,10 @@ fs.createReadStream('input.txt')
    .pipe(fs.createWriteStream('input.txt.gz'));
   
 console.log("File Compressed.")
+
+// Decompress the file input.txt.gz to input.txt
+fs.createReadStream('input.txt.gz')
+   .pipe(zlib.createGunzip())
+   .pipe(fs.createWriteStream('input.txt'));
+  
+console.log("File Decompressed.");
