88be5d9777b3c82ba80afa6ce1ac110a2084f455
PhyoThiHA0805 <scm.phyothihakyaw@gmail.com>
2022-12-07 16:59:29 +0630

    URL Building.

--- Day04/index.js
+++ Day04/index.js
@@ -16,6 +16,22 @@ app.all("/test", function (req, res) {
 
 app.use("/route", route);
 
+//// URL Building
+app.get("/:id", function (req, res) {
+  res.send("The id you specified is " + req.params.id);
+});
+
+app.get("/route/:name/:id", function (req, res) {
+  res.send("id: " + req.params.id + " and name: " + req.params.name);
+});
+
+app.get("/route/:id([0-9]{5})", function (req, res) {
+  res.send("id: " + req.params.id);
+});
+
+app.get("*", function (req, res) {
+  res.send("Sorry, this is an invalid URL.");
+});
 const server = app.listen(3000, "localhost", () => {
   const host = server.address().address;
   const port = server.address().port;
