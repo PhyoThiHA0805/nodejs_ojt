3b5513a12aa165dc254653ffa86e3cb71185e9d1
PhyoThiHA0805 <scm.phyothihakyaw@gmail.com>
2022-12-09 10:32:50 +0630

    Add one value to Table.

--- Day05/mysql/index.js
+++ Day05/mysql/index.js
@@ -28,10 +28,18 @@ con.connect((err) => {
     });
 
     // Add Column to existing Table
-      const addColumn = "ALTER TABLE customers ADD COLUMN phone VARCHAR(255)"
-      con.query(addColumn, (err) => {
-          if (err) throw err;
-          console.log("Column added successfully");
-      })
+    const addColumn = "ALTER TABLE customers ADD COLUMN phone VARCHAR(255)";
+    con.query(addColumn, (err) => {
+      if (err) throw err;
+      console.log("Column added successfully");
+    });
+
+    // Add one value to Table
+    var addValue =
+      "INSERT INTO customers (name, address) VALUES ('Company Inc', 'Highway 37')";
+    con.query(addValue, (err, result) => {
+      if (err) throw err;
+      console.log("1 record inserted");
+    });
   });
 });
