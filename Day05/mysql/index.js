2040c794bbefc2c6e7ddc2ca7e4a0d0bb10743b2
PhyoThiHA0805 <scm.phyothihakyaw@gmail.com>
2022-12-09 10:06:38 +0630

    Revert "Create Table."
    This reverts commit 6145b368220f5e1ca7d171235244e0235097dca5.

--- Day05/mysql/index.js
+++ Day05/mysql/index.js
@@ -1,30 +1,19 @@
-const mysql = require("mysql");
+const mysql = require('mysql');
 
 const con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "PhyoThiHA445442252#mysql",
+    host: "localhost",
+    user: "root",
+    password: "PhyoThiHA445442252#mysql"
 });
 
 // Create database
 con.connect((err) => {
-  if (err) throw err;
-  console.log("Connected!");
-  // con.query("CREATE DATABASE mytestdb", function (err, result) {
-  //     if (err)
-  //         throw err;
-  //     console.log("Database created");
-  // });
-
-  // Create Table
-  con.query("USE mytestdb", (err) => {
-    if (err) throw err;
-
-    const sql =
-      "CREATE TABLE customers (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), address VARCHAR(255))";
-    con.query(sql, (err) => {
-      if (err) throw err;
-      console.log("Table created successfully");
+    if (err)
+        throw err;
+    console.log("Connected!");
+    con.query("CREATE DATABASE mytestdb", function (err, result) {
+        if (err)
+            throw err;
+        console.log("Database created");
     });
-  });
 });
