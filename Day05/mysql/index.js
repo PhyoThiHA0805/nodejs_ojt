b6b2bdb210b802d334af33e2cb9b283fe571b949
PhyoThiHA0805 <scm.phyothihakyaw@gmail.com>
2022-12-09 09:42:33 +0630

    Create database;

--- Day05/mysql/index.js
+++ Day05/mysql/index.js
@@ -6,7 +6,14 @@ const con = mysql.createConnection({
     password: "PhyoThiHA445442252#mysql"
 });
 
+// Create database
 con.connect((err) => {
-    if (err) return console.log(err);
-    console.log("Database connected....")
-})
+    if (err)
+        throw err;
+    console.log("Connected!");
+    con.query("CREATE DATABASE mytestdb", function (err, result) {
+        if (err)
+            throw err;
+        console.log("Database created");
+    });
+});
