05f3a2aa063488305e6529c13afa6e71cbf89ec3
PhyoThiHA0805 <scm.phyothihakyaw@gmail.com>
2022-12-09 10:46:50 +0630

    formatted.

--- Day05/mysql/index.js
+++ Day05/mysql/index.js
@@ -39,31 +39,30 @@ con.connect((err) => {
       "INSERT INTO customers (name, address) VALUES ('Company Inc', 'Highway 37')";
     con.query(addValue, (err, result) => {
       if (err) throw err;
-      console.log("1 record inserted ","ID :", result.insertId);
+      console.log("1 record inserted ", "ID :", result.insertId);
     });
-      
-    // Add Multiple values to Table
-      const addMultiValues =
-          "INSERT INTO customers (name, address) VALUES ?";
-      const values = [
-        ['John', 'Highway 71'],
-        ['Peter', 'Lowstreet 4'],
-        ['Amy', 'Apple st 652'],
-        ['Hannah', 'Mountain 21'],
-        ['Michael', 'Valley 345'],
-        ['Sandy', 'Ocean blvd 2'],
-        ['Betty', 'Green Grass 1'],
-        ['Richard', 'Sky st 331'],
-        ['Susan', 'One way 98'],
-        ['Vicky', 'Yellow Garden 2'],
-        ['Ben', 'Park Lane 38'],
-        ['William', 'Central st 954'],
-        ['Chuck', 'Main Road 989'],
-        ['Viola', 'Sideway 1633']
-      ]
 
-      con.query(addMultiValues, [values], (err, result) => {
-          console.log("Number of records inserted: " + result.affectedRows);
-      })
+    // Add Multiple values to Table
+    const addMultiValues = "INSERT INTO customers (name, address) VALUES ?";
+    const values = [
+      ["John", "Highway 71"],
+      ["Peter", "Lowstreet 4"],
+      ["Amy", "Apple st 652"],
+      ["Hannah", "Mountain 21"],
+      ["Michael", "Valley 345"],
+      ["Sandy", "Ocean blvd 2"],
+      ["Betty", "Green Grass 1"],
+      ["Richard", "Sky st 331"],
+      ["Susan", "One way 98"],
+      ["Vicky", "Yellow Garden 2"],
+      ["Ben", "Park Lane 38"],
+      ["William", "Central st 954"],
+      ["Chuck", "Main Road 989"],
+      ["Viola", "Sideway 1633"],
+    ];
+
+    con.query(addMultiValues, [values], (err, result) => {
+      console.log("Number of records inserted: " + result.affectedRows);
+    });
   });
 });
