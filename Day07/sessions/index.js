680d01cfd998dc8b3c646158a206e83bf3d22fe2
PhyoThiHA0805 <scm.phyothihakyaw@gmail.com>
2022-12-12 11:16:25 +0630

    Session With file Store.

--- Day07/sessions/index.js
+++ Day07/sessions/index.js
@@ -1,23 +1,69 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const session = require("express-session");
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
+const fileStore = require("session-file-store")(session);
+const path = require("path");
 
 const app = express();
 
 app.use(cookieParser());
-app.use(session({ secret: "your-secret-key" }));
+app.use(
+  session({
+    name: "session-id",
+    secret: "secret",
+    saveUninitialized: false,
+    resave: false,
+    store: new fileStore(),
+  })
+);
 
-app.get("/", function (req, res) {
-  if (req.session.page_views) {
-    req.session.page_views++;
-    res.send("You visited this page " + req.session.page_views + " times");
+function auth(req, res, next) {
+  console.log(req.session);
+
+  if (!req.session.user) {
+    const authHeader = req.headers.authorization;
+    console.log(authHeader);
+    const err = new Error("You are not authenticated");
+    res.setHeader("WWW-Authenticate", "Basic");
+    err.status = 401;
+
+    next(err);
+
+    const auth = new Buffer.from(authHeader.split(" ")[1], "base64")
+      .toString()
+      .split(":");
+
+    // Reading username and password
+    const username = auth[0];
+    const password = auth[1];
+
+    if (username == "admin2" && password == "password") {
+      req.session.user = "admin2";
+      next();
+    } else {
+      // Retry incase of incorrect credentials
+      const err = new Error("You are not authenticated!");
+      res.setHeader("WWW-Authenticate", "Basic");
+      err.status = 401;
+      return next(err);
+    }
   } else {
-    req.session.page_views = 1;
-    res.send("Welcome to this page for the first time!");
+    if (req.session.user === "admin2") {
+      next();
+    } else {
+      const err = new Error("You are not authenticated!");
+      res.setHeader("WWW-Authenticate", "Basic");
+      err.status = 401;
+      return next(err);
+    }
   }
-});
+}
+// Middlewares
+app.use(auth);
+app.use(express.static(path.join(__dirname, "public")));
 
+// Server setup
 app.listen(3000, () => {
-  console.log("Server is Listening on port 3000");
+  console.log("Server is Starting");
 });
