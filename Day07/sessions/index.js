2df04f8f5bbae4c869008ce1d97b4f3fd4684f33
PhyoThiHA0805 <scm.phyothihakyaw@gmail.com>
2022-12-12 17:01:15 +0630

    Some Testing With Session.

--- Day07/sessions/index.js
+++ Day07/sessions/index.js
@@ -18,6 +18,49 @@ app.use(
   })
 );
 
+// function auth(req, res, next) {
+//   if (!req.session.user) {
+//     // Check if the authorization header exists and has the correct format
+//     const authHeader = req.headers.authorization;
+//     if (!authHeader || !authHeader.startsWith("Basic ")) {
+//       const err = new Error("Missing or invalid authorization header");
+//       err.status = 400; // Bad Request
+//       return next(err);
+//     }
+
+//     // Extract the username and password from the authorization header
+//     const auth = new Buffer.from(authHeader.split(" ")[1], "base64")
+//       .toString()
+//       .split(":");
+//     const username = auth[0];
+//     const password = auth[1];
+
+//     // Check the username and password against the expected values
+//     if (username == "admin2" && password == "password") {
+//       req.session.user = "admin2";
+//       next();
+//     } else {
+//       // Send a 401 Unauthorized error if the credentials are incorrect
+//       const err = new Error("Incorrect username or password");
+//       res.setHeader("WWW-Authenticate", "Basic");
+//       err.status = 401;
+//       return next(err);
+//     }
+//   } else {
+//     // Check if the user in the session is the expected user
+//     if (req.session.user === "admin2") {
+//       next();
+//     } else {
+//       // Send a 403 Forbidden error if the user in the session is not the expected user
+//       const err = new Error("You are not authorized to access this resource");
+//       err.status = 403;
+//       return next(err);
+//     }
+//   }
+// }
+// Middlewares
+// app.use(auth);
+
 function auth(req, res, next) {
   console.log(req.session);
 
@@ -34,6 +77,7 @@ function auth(req, res, next) {
       .toString()
       .split(":");
 
+    console.log('authHeader :', authHeader);
     // Reading username and password
     const username = auth[0];
     const password = auth[1];
@@ -59,10 +103,11 @@ function auth(req, res, next) {
     }
   }
 }
-// Middlewares
-app.use(auth);
 app.use(express.static(path.join(__dirname, "public")));
 
+app.get('/',auth ,(req, res) => {
+  res.send('Hello');
+})
 // Server setup
 app.listen(3000, () => {
   console.log("Server is Starting");
